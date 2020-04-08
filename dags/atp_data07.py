b08e954131c5e872920681c145873f655fe87219
Chris Chavez <chrischavezdataengineer@gmail.com>
2020-04-07 22:46:32 -0400

    cleaned repo, working on atp_data07 to create db and import submission.csv

--- dags/atp_data07.py
+++ dags/atp_data07.py
@@ -36,13 +36,37 @@ dag = DAG(
 def get_kaggle():
     url = 'https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data'
     response = requests.get(url)
-    with open('test.csv', 'wb') as f:
+    with open('sample_submission.csv', 'wb') as f:
         f.write(response.content)
 
 def unzip():
     zf = zipfile.ZipFile('/Users/cchavez/dev/AirflowProject/house-prices-advanced-regression-techniques/house-prices-advanced-regression-techniques.zip')
-    df = pd.read_csv(zf.open('Data.csv'), encoding='ISO-8859-1')
-    df.to_csv('/Users/cchavez/dev/AirflowProject/house-prices-advanced-regression-techniques/Data.csv')
+    df = pd.read_csv(zf.open('sample_submission.csv'), encoding='ISO-8859-1')
+    df.to_csv('/Users/cchavez/dev/AirflowProject/house-prices-advanced-regression-techniques/sample_submission.csv')
+
+# test connection to postgres
+def connect_pst():
+    conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
+
+# created sample_submission table
+def create_tbl_sample():
+    conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE sample(
+        Id varchar(255),
+        SalePrice numeric
+    )
+    """)
+    conn.commit()
+
+def insert_csv_sample():
+    conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
+    cur = conn.cursor()
+    with open("/Users/cchavez/dev/AirflowProject/house-prices-advanced-regression-techniques/sample_submission.csv", 'r') as f:
+        next(f)
+        cur.copy_from(f, 'sample', sep=',')
+        conn.commit()
 
 T_I = BashOperator(
     task_id='run_kaggle_api',
@@ -57,4 +81,25 @@ T_II = PythonOperator(
     dag=dag,
 )
 
-T_I >> T_II 
+T_III = PythonOperator(
+    task_id='connect_pst',
+    provide_context=False,
+    python_callable=connect_pst,
+    dag=dag,
+)
+
+T_IV = PythonOperator(
+    task_id='create_tbl_sample',
+    provide_context=False,
+    python_callable=create_tbl_sample,
+    dag=dag,
+)
+
+T_V = PythonOperator(
+    task_id='insert_csv_sample',
+    provide_context=False,
+    python_callable=insert_csv_sample,
+    dag=dag,
+)
+
+T_I >> T_II >> T_III >> T_IV >> T_V
